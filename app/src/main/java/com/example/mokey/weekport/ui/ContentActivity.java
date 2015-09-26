43f0e97ed782425458529f2cbb4d9e3e45adb377
obama with hxb <xiaoxiaobindeemail@gmail.com>
2015-09-26 22:19:53 +0800

    删除多余activity

--- app/src/main/java/com/example/mokey/weekport/ui/ContentActivity.java
+++ app/src/main/java/com/example/mokey/weekport/ui/ContentActivity.java
@@ -1,35 +1,41 @@
 package com.example.mokey.weekport.ui;
 
 import android.os.Bundle;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
 import android.view.Menu;
 import android.view.MenuItem;
 
 import com.example.mokey.weekport.R;
 import com.example.mokey.weekport.ui.core.BaseActivity;
+import com.example.mokey.weekport.ui.fragment.WeekPortFragment;
 
 public class ContentActivity extends BaseActivity {
+
+    private FragmentManager mFragmentManger;
+    private WeekPortFragment mWeekPortFragment;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_content);
+        mFragmentManger = getSupportFragmentManager();
+        FragmentTransaction transaction = mFragmentManger.beginTransaction();
+        mWeekPortFragment = WeekPortFragment.newInstance(null, null);
+        transaction.add(R.id.contentContainer, mWeekPortFragment, mWeekPortFragment.getPrivateTag());
+        transaction.commit();
     }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.menu_content, menu);
         return true;
     }
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
         int id = item.getItemId();
 
-        //noinspection SimplifiableIfStatement
         if (id == R.id.action_settings) {
             return true;
         }
